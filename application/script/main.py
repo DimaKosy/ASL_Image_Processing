18982c3d83db5b8b298d6f6d2d2d32aa35a05280
Dmytro Kosynskyy <dmytro.kosynskyy@stud.h-da.de>
2024-10-09 14:36:55 +0100

    first isolation steps

---
+++ application/script/main.py
@@ -0,0 +1,112 @@
+import math
+import cv2
+import easygui
+import matplotlib.pyplot
+import numpy as np
+from matplotlib import pyplot as plt
+import preprocess
+import threading
+
+
+running:bool = True
+res = 20
+
+def echo(args = ""):
+    print(args)
+
+def shutdown(args = ""):
+    global running
+
+    print("exiting...")
+    running = False
+    pass
+
+cmd_list = {
+    "echo":echo,
+    "exit":shutdown,
+    "y_thresh":preprocess.set_Y_thresh,
+    "cr_thresh":preprocess.set_Cr_thresh,
+    "cb_thresh":preprocess.set_Cb_thresh,
+    "thresh":preprocess.set_threst_state,
+    "values":preprocess.echo_tresh
+}
+
+
+
+def async_input():
+    while running:
+
+        cmd = input("/")
+        cmd_split = cmd.split(" ",1)
+
+        cmd_fun = cmd_list.get(cmd_split[0])
+
+        if cmd_fun == None:
+            continue
+        
+        if len(cmd_split) <= 1:
+            cmd_fun()
+            continue
+        
+        # print(*cmd_split[1:])
+
+        try:
+            cmd_fun(*cmd_split[1:])
+        except Exception as e:
+            print(e)
+
+    pass
+
+def main():
+    
+
+    input_thread = threading.Thread(target=async_input,args=())
+    input_thread.start()
+    # fp = easygui.fileopenbox()
+
+    # frame = cv2.imread(fp)
+
+    cam = cv2.VideoCapture(0)
+    # cam.set(cv2.CAP_PROP_AUTO_EXPOSURE, 1) #disable auto exposure
+    # cam.set(cv2.CAP_PROP_AUTOFOCUS, 0) #disable autofocus
+
+    cam.set(cv2.CAP_PROP_FRAME_WIDTH,16*res)
+    cam.set(cv2.CAP_PROP_FRAME_HEIGHT,9*res)
+    # cam.set(cv2.CAP_PROP_EXPOSURE,1)
+    # frame = preprocess.CreateHist(frame)
+
+    while(running):
+        ret, frame = cam.read()
+        frame = cv2.flip(frame,1)
+
+        Y, CR, CB = preprocess.Isolate(frame)
+        
+        # pre = cv2.multiply(CB,CR)
+        pre = cv2.bitwise_and(CR,CB)
+        post_Y = cv2.multiply(pre,Y,scale=0.01)
+        # post = cv2.merge([post_Y,post_Y, post_Y
+        ret, post = cv2.threshold(post_Y,1,255,cv2.THRESH_BINARY)
+        # print(type(post))
+
+        post = cv2.merge([post,post, post])
+        post = cv2.multiply(frame, post, scale= 1/255)
+        # post = cv2.cvtColor(post, cv2.COLOR_YCR_CB2BGR)
+
+        cv2.imshow('Camera', post)
+        cv2.imshow('dist', post_Y)
+        cv2.imshow('Y', Y)
+        cv2.imshow('CR', CR)
+        cv2.imshow('CB', CB)
+        
+        
+
+        # plt.show(block = False) 
+        key = cv2.waitKey(50)
+        # if key == 27:
+        #     break
+        # break
+
+    input_thread.join()
+
+if __name__ == "__main__":
+    main()
---
+++ application/script/preprocess.py
@@ -0,0 +1,131 @@
+import math
+import cv2
+import easygui
+import matplotlib.pyplot
+import numpy as np
+from matplotlib import pyplot as plt
+
+
+def CreateHist(frame):
+    # frame = cv2.cvtColor(frame,cv2.COLOR_GRAY2BGR)
+    frame = cv2.cvtColor(frame,cv2.COLOR_BGR2YUV)
+
+    Block = [Y,CR,CB] = cv2.split(frame)
+
+    for i in  Block:
+        values = i.ravel()
+        # i = cv2.cvtColor(i,cv2.COLOR_BGR2GRAY)
+        plt.hist(values,bins=256,range=[0,256], label=f'{i}')
+        plt.show()
+
+
+    return frame
+
+
+T_state = "Binary"
+T =  cv2.THRESH_TOZERO
+TI = cv2.THRESH_TOZERO_INV
+
+Y_Thresh_min = 0
+Y_Thresh_max = 255
+Cr_Thresh_min = 136
+Cr_Thresh_max = 155
+Cb_Thresh_min = 100
+Cb_Thresh_max =  125
+
+def echo_tresh():
+    print("Y_min {}\nY_max {}\nU_min {}\nU_max {}\nV_min {}\nV_max {}".format(
+        Y_Thresh_min,
+        Y_Thresh_max,
+        Cr_Thresh_min,
+        Cr_Thresh_max,
+        Cb_Thresh_min,
+        Cb_Thresh_max
+    ))
+
+def set_threst_state(thresh_list):
+    global T
+    global TI
+
+    
+    thresh_list = thresh_list.split(" ")
+    arg, *_ = thresh_list
+
+    if(arg == "Binary"):
+        # T =  cv2.THRESH_BINARY
+        TI = cv2.THRESH_BINARY_INV
+        return
+    
+    T =  cv2.THRESH_TOZERO
+    TI = cv2.THRESH_TOZERO_INV
+
+
+
+def set_Y_thresh(thresh_list):
+    print(thresh_list)
+    if(type(thresh_list)==str):
+        thresh_list = thresh_list.split(" ")
+        print(thresh_list)
+        thresh_list = [eval(i) for i in thresh_list]
+    
+
+    global Y_Thresh_min
+    global Y_Thresh_max
+    
+    Y_Thresh_min,Y_Thresh_max, *_ = thresh_list
+
+def set_Cr_thresh(thresh_list):
+    if(type(thresh_list)==str):
+        thresh_list = thresh_list.split(" ")
+        thresh_list = [eval(i) for i in thresh_list]
+
+    global Cr_Thresh_min
+    global Cr_Thresh_max
+    
+    Cr_Thresh_min,Cr_Thresh_max, *_ = thresh_list
+
+def set_Cb_thresh(thresh_list):
+    if(type(thresh_list)==str):
+        thresh_list = thresh_list.split(" ")
+        thresh_list = [eval(i) for i in thresh_list]
+
+    global Cb_Thresh_min
+    global Cb_Thresh_max
+    
+    Cb_Thresh_min,Cb_Thresh_max, *_ = thresh_list
+
+
+
+def Isolate(frame):
+    frame = cv2.cvtColor(frame,cv2.COLOR_BGR2YCrCb)
+
+    Block = [Y,CR,CB] = cv2.split(frame)
+    
+    # CR = cv2.resize(CR,(0,0),fx=0.2, fy=0.2)
+    # CB = cv2.resize(CB,(0,0),fx=0.2, fy=0.2)
+
+    cv2.threshold(Y,Y_Thresh_min,255,T, Y)
+    cv2.threshold(Y,Y_Thresh_max,255,TI, Y)
+    cv2.threshold(CR,Cr_Thresh_min,255,T, CR)
+    cv2.threshold(CR,Cr_Thresh_max,255,TI,CR)
+    cv2.threshold(CB,Cb_Thresh_min,255,T, CB)
+    cv2.threshold(CB,Cb_Thresh_max,255,TI, CB)
+
+    # CR = cv2.GaussianBlur(CR,(3,3),0.5)
+    # CB = cv2.GaussianBlur(CB,(3,3),0.5)
+
+    # CR = cv2.resize(CR,(0,0),fx=5, fy=5)
+    # CB = cv2.resize(CB,(0,0),fx=5, fy=5)
+    
+    # values = Y.ravel()
+    # plt.hist(values,bins=256,range=[0,256],label="Y")
+    # values = CR.ravel()
+    # plt.hist(values,bins=256,range=[0,256],label="U")
+    # values = CB.ravel()
+    # plt.hist(values,bins=256,range=[0,256],label="V")
+
+
+    # frame = cv2.merge([Y,CR,CB])
+    # frame = cv2.cvtColor(frame,cv2.COLOR_YCrCb2BGR)
+
+    return Y, CR, CB
